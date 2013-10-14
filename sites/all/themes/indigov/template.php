ae930b166141be8394d2fbfa0f529b5d8e324a05
Gerrit Vos <gevo27@gmail.com>
2013-10-14 22:40:13 +0200

    Slicing

--- sites/all/themes/indigov/template.php
+++ sites/all/themes/indigov/template.php
@@ -130,3 +130,17 @@ function indigov_preprocess_block(&$variables, $hook) {
   //}
 }
 // */
+
+function indigov_menu_link(array $variables) {
+  $element = $variables['element'];
+  
+  $element['#localized_options']['html'] = TRUE;
+  
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
